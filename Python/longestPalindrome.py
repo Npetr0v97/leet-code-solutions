b4ba657764a19471f5f73ef57ad9db8b64a28b86
Nikolay Petrov <nikp@OSX-NIKP-N-BG.local>
2025-08-16 02:01:44 +0300

    bplate

---
+++ Python/longestPalindrome.py
@@ -0,0 +1,12 @@
+# Given a string s, return the longest palindromic substring in s.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        return s
+
+
+x = Solution()
+
+
+str = "test"
+print(x.longestPalindrome(str))
