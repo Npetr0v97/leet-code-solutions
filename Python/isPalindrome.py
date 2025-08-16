828a1d02b8a6ee6c56cd8625f8ebb4a7e2238ee5
Nikolay Petrov <nikp@OSX-NIKP-N-BG.local>
2025-08-17 00:29:18 +0300

    b-plate for isPalindrome

---
+++ Python/isPalindrome.py
@@ -0,0 +1,12 @@
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        return x
+
+
+x = Solution()
+
+num1 = 121
+
+print(x.isPalindrome(num1))
