3fc61635092e0485195ae45d7196c870cd4a7cb6
Nikolay Petrov <nikp@OSX-NIKP-N-BG.local>
2025-08-18 16:28:49 +0300

    isPalindrome solution

--- Python/isPalindrome.py
+++ Python/isPalindrome.py
@@ -1,12 +1,21 @@
 # Given an integer x, return true if x is a palindrome, and false otherwise.
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-
-        return x
+        string_num = str(x)
+        answer = True
+        for i, num in enumerate(string_num):
+            index_mirror_item = len(string_num) - i - 1
+            # print(string_num[i], string_num[index_mirror_item])
+            if string_num[i] != string_num[index_mirror_item]:
+                answer = False
+                break
+            if i == len(string_num) // 2:
+                break
+        return answer
 
 
 x = Solution()
 
-num1 = 121
+num1 = 0
 
 print(x.isPalindrome(num1))
